65a15b7d3778e2ecd8aad450d36cc0c87a477296
Mauricio <vemauricio20@gmail.com>
2024-08-14 15:22:51 -0600

    Primer commit del proyecto

---
+++ src/test/java/hn/unah/lenguajes/proyecto/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.proyecto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
